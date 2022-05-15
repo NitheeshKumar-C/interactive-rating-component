f0e469bbf73780e4824aefe41009f4e848560876
NitheeshKumar-C <cnitheeshkumar20@gmail.com>
2022-05-15 18:02:09 +0530

    c4: modified

--- js/app.js
+++ js/app.js
@@ -1,16 +1,37 @@
 var value;
+var oldValue;
 var error=document.getElementById('error');
-        
+var a;
 function ratingValue(v){
     value=v;
+    for(let i=1;i<=5;i++){
+    
+        if(value==i.toString())
+        {
+            console.log("yes "+i);
+            a=document.getElementById(i);
+            a.style.backgroundColor="var(--primaryOrange)";
+            a.style.color="white";
+            oldValue=value;
+        }
+        else{
+            a=document.getElementById(i);
+            a.style.backgroundColor="hsl(212, 16%, 21%)";
+            a.style.color="var(--neutralLightGrey)";
+        }
+    }
+
+    
     error.style.display="none";
 }
 
 function afterRating(){
+
     if(value==undefined){
         error.innerHTML='*Please select the rating';
     }
-    else{
+    else
+    {
         var bRating=document.getElementById('br');
         bRating.style.display='none';
 
